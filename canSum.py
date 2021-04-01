5db8226a7765b07641d22f388421e99261494149
Jcupzz <jcupzz255@gmail.com>
2021-04-01 15:35:30 +0530

    Fixed error

--- canSum.py
+++ canSum.py
@@ -2,14 +2,15 @@
 # Write a function canSum(targetSum,numbers) that takes in a targetSum and an array of numbers as arguments. The   function should return a boolean indicating whether or not it's possible to generate the targetSum using numbers from the array. You may use an element of the array as many as times as needed. You may assume that all input numbers are non-negative. 
 
 def canSum(targetSum,numbers,dict = {}):
-    if(targetSum == 0):
+    if(targetSum in dict):
+        return dict[targetSum]
+    if(targetSum is 0):
         return True
     if (targetSum<0):
         return False
-    if(targetSum in dict):
-        return dict[targetSum]
     for num in numbers:
         remainder = targetSum - num
+        print(remainder)
         if(canSum(remainder,numbers,dict)==True):
             dict[targetSum] = True
             return True
@@ -19,4 +20,5 @@ def canSum(targetSum,numbers,dict = {}):
 print(canSum(7,[2,3]))
 print(canSum(7,[5,3,4,7]))
 print(canSum(7,[2,4]))
-print(canSum(300,[2,7]))
+print(canSum(8,[2,3,5]))
+print(canSum(300,[7,14]))
